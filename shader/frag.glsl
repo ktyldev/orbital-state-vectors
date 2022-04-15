84c37f2ece332d0770f9dcc95ad43cb0ca00dc43
ktyl <me@ktyl.dev>
2022-04-15 23:47:21 +0100

    fade colour with distance

--- shader/frag.glsl
+++ shader/frag.glsl
@@ -2,9 +2,14 @@
 
 uniform vec3 color;
 
+in vec3 vpos;
+
 out vec4 outColor;
 
 void main()
 {
-    outColor = vec4(color, 1.0);
+    float max = 2.0;
+    float d = smoothstep(1, 0, length(vpos) / max);
+
+    outColor = vec4(color, 1.0) * d;
 }
--- shader/vert.glsl
+++ shader/vert.glsl
@@ -6,9 +6,14 @@ uniform mat4 projection;
 
 in vec3 position;
 
+out vec3 vpos;
+
 void main()
 {
+    vec3 p = position;
     mat4 camera = projection * view;
 
-    gl_Position = camera * model * vec4(position, 1.0);
+    gl_Position = camera * model * vec4(p, 1.0);
+
+    vpos = p;
 }
